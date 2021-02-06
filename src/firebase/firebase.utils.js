da003aad3b212ea54b64ba7226f436bb8feb46d0
Rita Dias <anarita151@gmail.com>
2021-02-06 23:16:53 +0100

    feat: Sign in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
   appId: "1:517226538647:web:290522c84234e082636bd2",
   measurementId: "G-H1DE2BTSQ1"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // CRUD - set / get / update / delete
+  const snapShot = await userRef.get(); // get
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
