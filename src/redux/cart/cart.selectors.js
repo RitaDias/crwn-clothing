8e9d03294776f0660e105d789490c78f098bacd9
Rita Dias <anarita151@gmail.com>
2021-02-07 14:44:43 +0100

    refactor: Update application using memoised selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,20 @@
+import { createSelector } from "reselect";
+
+// selectors are getters in vue
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
